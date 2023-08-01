363c15acee2499ce1bd89d879e0656b66aa9908c
Tata_Babar <tbabarykina@gmail.com>
2023-08-01 20:36:38 +0400

    Задача 23 таблица кубов

---
+++ Task00/Task23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напищите программу, которая
+// 1. принимает на вход число (N)
+// 2. и выдает таблицу кубов чисел от 1 до N
+// 5 ->
+// 1|1
+// 2|8
+// 3|27
+// 4|64
+// 5|125
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+}
+CubeTable (number);
+
+void CubeTable (int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int cube = i*i*i;
+        Console.WriteLine($"{i}   {cube}");
+    }
+}
+
